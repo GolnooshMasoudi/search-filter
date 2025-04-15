6bcd8a3f06b3196b881d38578b7e536117933f31
GolnooshMasoudi <golnoosh.masoudi@gmail.com>
2025-04-15 16:07:16 +0200

    render products

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const btns = document.querySelectorAll(".btn");
+const btns = document.querySelectorAll(".btn");
 const productsDOM = document.querySelector(".products-center");
 // const storeProducts = document.querySelectorAll(".product");
 const searchInput = document.querySelector("#search");
@@ -51,41 +51,12 @@ searchInput.addEventListener("input", (e) => {
   renderProducts(allProductsData, filters);
 });
 
-// function renderProducts(products, _filters) {
-//   const filteredProducts = products.filter((p) => {
-//     return p.title.toLowerCase().includes(_filters.searchItems.toLowerCase());
-//   });
-//   // console.log(filteredProducts);
-//   //   return filteredProducts;
-//   productsDOM.innerHTML = "";
-//   filteredProducts.forEach((item) => {
-//     const productDiv = document.createElement("div");
-//     // const className = `product ${item.class}`;
-//     productDiv.classList.add("product");
-//     productDiv.innerHTML = `<div class="img-container">
-//     <img src=${item.image} class="product-img" />
-//     </div>
-//     <div class="product-desc">
-//       <p class="product-price">$ ${item.price}</p>
-//       <p class="product-title">${item.title}</p>
-//     </div>`;
-//     productsDOM.appendChild(productDiv);
-//   });
-// }
-
-// searchInput.addEventListener("input", (e) => {
-//   console.log(e.target.value);
-//   filters.searchItems = e.target.value;
-//   renderProducts(allProductsData, filters);
-// });
-
-// // filter based on groups :
-// btns.forEach((btn) => {
-//   btn.addEventListener("click", (e) => {
-//     const filter = e.target.dataset.filter;
-//     // e.target.classList.add("active");
-//     // console.log(e.target.innerText);
-//     filters.searchItems = filter;
-//     renderProducts(allProductsData, filters);
-//   });
-// });
+//filter base on groups:
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const filter = e.target.dataset.filter;
+    console.log(e.target.dataset.filter);
+    filters.searchItems = filter;
+    renderProducts(allProductsData, filters);
+  });
+});
